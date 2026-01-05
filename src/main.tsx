9b701f829104bdd089305cf253ad5af6896cba78
4nxiouz <zizplenter@gmail.com>
2026-01-05 11:34:42 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // เพิ่มบรรทัดนี้
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter> {/* ครอบ App ด้วย BrowserRouter */}
-      <App />
-    </BrowserRouter>
+    {/* เอา BrowserRouter ออกจากตรงนี้ เพราะเราไปใส่ใน App.tsx แล้ว */}
+    <App />
   </StrictMode>
 );
