20908adfc488401011552b3ab05c55d929b74082
4nxiouz <zizplenter@gmail.com>
2026-01-05 15:58:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  // ห้ามใส่ base: './' ถ้าไม่ได้ใช้ HashRouter หรือโฟลเดอร์ย่อย
+  // ให้ลบ base ออก หรือใส่เป็น '/'
+  base: '/', 
+})
